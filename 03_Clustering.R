5bfc4c94c5f4f695da29f62e93d60eb7714760a0
Martin Scheerer <martin-scheerer97@web.de>
2021-12-04 15:34:47 +0100

    Clustering

--- 03_Clustering.R
+++ 03_Clustering.R
@@ -14,17 +14,75 @@ if (!require("tidyverse")) install.packages("tidyverse")
 if (!require("dplyr")) install.packages("dplyr")
 if (!require("tibble")) install.packages("tibble")
 if (!require("ggplot2")) install.packages("ggplot2")
-if (!require("stringr")) install.packages("stringr")
-if (!require("stringi")) install.packages("stringi")
-if (!require("lubridate")) install.packages("lubridate")
+if (!require("cluster")) install.packages("cluster")
+if (!require("ggrepel")) install.packages("ggrepel")
+if (!require("factoextra")) install.packages("factoextra")
 
 library(tidyverse)
 library(dplyr)
 library(tibble)
 library(ggplot2)
-library(stringr)
-library(stringi)
-library(lubridate)
+library(cluster)
+library(ggrepel)
+library(factoextra)
+
+# # prepare data for clustering: mean 0 and std 1 to normalize data
+# 
+# measures <- cross_sec[, c(1:18, 40:41)]
+# rownames(measures) <- cross_sec$Country
+# 
+# colnames(measures[, c(-1:-3, -19:-20)])
+# distance <- as.data.frame(lapply(measures[, c(-1:-3, -19:-20)], scale))
+#   
+# # k-means clustering)
+# 
+# fviz_nbclust(distance, kmeans, method = "wss")
+# 
+# set.seed(123)
+# kmeans <- kmeans(distance, 4, nstart = 25)
+# clusters <- data.frame(cluster = kmeans$cluster, row.names = NULL)
+# 
+# measures$cluster <- as.factor(as.character(clusters$cluster))
+
+#### Case-Death Plot with cluster coloring
+
+# cases_deaths_plot <- ggplot(measures, aes(`Cases Per 10 Million`, `Deaths Per 100 Thousand`)) +
+#   geom_jitter(height = 10, size = 6, aes(colour = cluster)) + 
+#   scale_color_viridis_d(alpha = 0.45) +
+#   geom_smooth(method = "lm", se = FALSE) + 
+#   geom_text_repel(aes(label = Country), angle = 20, nudge_y  = ifelse(measures$Country == "Australia", 10, 0), 
+#             nudge_x = ifelse(measures$Country == "Korea, Rep.", 250000, 0)) +
+#   scale_x_continuous(breaks = seq(0, 3000000, 500000)) + 
+#   scale_y_continuous(breaks = seq(0, 400, 50)) + 
+#   labs(x = "Total cases per 10 million population", y = "Total deaths per 100 thousand population") +
+#   theme_classic() +
+#   theme(legend.position = c(0.9, 0.5)) +
+#   guides(colour = guide_legend(title = "Cluster\nmembership\n", title.position = "top"), 
+#          label.hjust = 0.5) 
+# 
+# ggsave("figures/deaths_cases_cluster.png", width = 14) 
+
+#### Hierarchical Clustering
+
+cross_sec <- readRDS("Output/cross_section_data.rds")
+
+measures <- scale(cross_sec[, 4:16])
+rownames(measures) <- cross_sec$Country
+
+distance <- dist(measures, method = "euclidean")
+
+## Clustering: Agglomerative - Average/Single Linkage
+
+hc <- hclust(distance, method = "single")
+hc1 <- as.dendrogram(hc)
+
+png(file = "figures/deaths_cases_cluster.png", width = 800, height = 650)
+
+nodePar <- list(lab.cex = 0.6, pch = c(19, 19), cex = 0.8, col = "blue")
+plot <- plot(hc1,  xlab = "Height",nodePar = nodePar, horiz = TRUE)
+
+dev.off()
+
 
 
 
