d51ca9095a3fb4601accc6871ea81817f8371872
Martin Scheerer <martin-scheerer97@web.de>
2022-01-03 12:06:47 +0100

    adjusted plots

--- 03_Data_Visualization.R
+++ 03_Data_Visualization.R
@@ -205,12 +205,13 @@ cases_deaths_plot <- ggplot(cor_cont, aes(`Cases Per 10 Million`, `Deaths Per 10
   geom_point(size = 8, aes(colour = Continent)) + 
   scale_color_viridis_d(alpha = 0.65) +
   geom_smooth(method = "lm", se = FALSE) + 
-  geom_text_repel(aes(label = Country), angle = 20, nudge_x = ifelse(cor_cont$Country == "Japan", 2, 0)) +
+  geom_text_repel(aes(label = Country), angle = 20, nudge_x = ifelse(cor_cont$Country == "Japan", 2, 0), 
+                  nudge_y = ifelse(cor_cont$Country == "Korea, Rep.", 2, 0)) +
   scale_x_continuous(breaks = seq(0, 3000000, 500000)) + 
   scale_y_continuous(breaks = seq(0, 400, 50)) + 
   labs(x = "Total cases per 10 million population", y = "Total deaths per 100 thousand population") +
   theme_classic() +
-  theme(legend.position = c(0.9, 0.5)) +
+  theme(legend.position = c(0.9, 0.3)) +
   guides(colour = guide_legend(title = "Continent\n", title.position = "top"), 
          label.hjust = 0.5) 
 
@@ -250,7 +251,7 @@ measure_bar <- data.frame(t(measure_desc))
 measure_bar$Measure <- rownames(measure_bar)
 
 rownames(measure_bar) <- NULL
-measure_bar <- measure_bar[-c(14:15), ]
+measure_bar <- measure_bar[-c(13:15), ]
 
 measure_barplot <- ggplot(measure_bar, aes(x = reorder(Measure, Mean), y = Mean)) +
   geom_bar(stat = "identity", fill = "grey") + 
@@ -260,7 +261,7 @@ measure_barplot <- ggplot(measure_bar, aes(x = reorder(Measure, Mean), y = Mean)
   theme_classic() +
   theme(axis.title.x = element_text(vjust = -1), axis.title.y = element_text(vjust = 2))
 
-ggsave("figures/measure_days_barplot.png")  
+ggsave("figures/measure_days_barplot.png", width = 10)  
 
 #### World maps: total cases and total deaths
 
